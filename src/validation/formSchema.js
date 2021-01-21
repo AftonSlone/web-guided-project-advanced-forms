6ea1594560dc027895a2939bad105757871160b4
Afton Slone <afton.slone@gmail.com>
2021-01-21 18:22:25 -0500

    Finished Guided Project

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,19 @@
 // Here goes the schema for the form
+import * as yup from "yup";
+export default yup.object().shape({
+  username: yup
+    .string()
+    .required("username is required")
+    .min(3, "username must be 3 characters"),
+  email: yup
+    .string()
+    .email("must be a valid email address")
+    .required("email is required"),
+  role: yup
+    .string()
+    .oneOf(["tl", "instructor", "alumni", "student"], "role is required"),
+  civil: yup.string().oneOf(["married", "single"], "Civil status is required"),
+  coding: yup.boolean(),
+  reading: yup.boolean(),
+  hiking: yup.boolean(),
+});
